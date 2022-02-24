bd6f821dd0ae9e4fe7d2b88b59131d3ee5ae860f
Farhaan Shaik <farhaanfsk@gmail.com>
2022-02-24 12:46:16 +0530

    mysql set up

---
+++ Autoparking-Web/src/main/java/com/fsk/microservices/autoparking/web/domain/SlotBooking.java
@@ -0,0 +1,43 @@
+package com.fsk.microservices.autoparking.web.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Future;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@Setter
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class SlotBooking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "emp_id")
+    private long empId;
+    @Column(name = "slot_id")
+    private long slotId;
+    @Column(name = "vehicle_id")
+    private long vehicleId;
+    @Future
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @Column(name = "start_time")
+    private LocalDateTime startTime;
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(name = "end_time")
+    @Future
+    private LocalDateTime endTime;
+}
